43a13648b9f2c7a561f63117d5391df996a760bd
Leviathaned <levi.shelleye@gmail.com>
2023-07-14 22:34:24 -0400

    includes player sprites and scenes

---
+++ Player/player.gd
@@ -0,0 +1,40 @@
+extends Area2D
+signal hit
+
+@export var speed = 200
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	#must be deferred as we can't change physics properties on a physics callback
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
