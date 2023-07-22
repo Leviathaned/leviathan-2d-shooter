6fd95b4217b8eb2c6baba46c4d2f6d3cba2a4fc1
Leviathaned <levi.shelleye@gmail.com>
2023-07-21 23:42:15 -0400

    Adds the ability for the player to shoot bullets

---
+++ Player/Shot/shot.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var speed = 750
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	
+#This function deletes the bullet if it hits anything at all.
+func _on_Bullet_body_entered(body):
+	queue_free()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
